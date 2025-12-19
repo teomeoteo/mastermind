c572c045cd479c871c65ddbefccf4d403081896b
teomac <liquidinobusiness@gmail.com>
2025-12-19 04:02:12 +0100

    Add UI, fix colors, improve logic

--- sketch.rb
+++ sketch.rb
@@ -1,34 +1,66 @@
 COLORS = {
   'red'    => "\e[31m●\e[0m",
   'green'  => "\e[32m●\e[0m",
-  'blue'   => "\e[34m●\e[0m",
+  'blue'   => "\e[38;5;21m●\e[0m",
   'yellow' => "\e[33m●\e[0m",
-  'purple' => "\e[35m●\e[0m",
+  'purple' => "\e[38;5;57m●\e[0m",
   'orange' => "\e[38;5;208m●\e[0m",
 }.freeze
 
 class Game
-  attr_accessor :board, :secret_code
-  def initialize(board)
+  attr_accessor :board, :secret_code, :player
+  def initialize(board, player = Player.new)
     @board = board
     @secret_code = set_code
+    @player = player
   end
 
   def start
+    board.rows[0].pointer = true
+    board.render
+    board.rows.each do |row|
+      row.pointer = true
+      board.length.times do |i|
+        puts "Enter a color"
+        puts "(red, green, blue, yellow, purple orange)"
+        print "\n"
+        color = player.input
+        row.holes[i].update_state(color)
+        print ("\n")
+        board.render
+      end
+      row.pointer = false
+    end
   end
 
   def set_code
     COLORS.keys.sample(board.rows[0].holes.length)
   end
+
+  def check
+  end
 end
 
 class Player
+  def input
+    color = ''
+    loop do
+      color = gets.chomp.to_s.downcase
+      if (COLORS.keys.include?(color))
+        break
+      else
+        puts "Invalid color"
+      end
+    end
+    color
+  end
 end
 
 class Row
-  attr_accessor :holes
+  attr_accessor :holes, :pointer
   def initialize(hole_count) # number of holes
     @holes = make(hole_count.to_int)
+    @pointer = false
   end
 
   def make(holes)
@@ -42,6 +74,13 @@ class Row
   def render
     holes.each do |hole|
       hole.render
+    end
+    if (pointer)
+      print " <---"
+      print " Colors: "
+      COLORS.each do |color, value|
+        print value + " "
+      end
     end
     print "\n"
   end
@@ -62,32 +101,35 @@ class Row
 end
 
 class Board
-  attr_accessor :rows
-  def initialize(row = Row.new(4))
-    @rows = make(row)
+  attr_accessor :rows, :feedback, :length
+  def initialize(feedback = Feedback.new(4))
+    @rows = make
+    @feedback = feedback
+    @length = feedback.holes.length
   end
   def render
+    feedback.render
     rows.each do |row|
       row.render
     end
   end
 
-  def make(row)
-    Array.new(12) {row}
+  def make
+    Array.new(12) {Row.new(4)}
   end
 end
 
-class Player
-  def input(board)
-    puts "Choose 4 colors in order 1 by 1"
+class Feedback < Row
+  def render
+    holes.each do |hole|
+      hole.render
+    end
+    print "\n"
+    puts "-------"
   end
 end
 
-class Feedback
-  def initialize(row = Row.new(4))
-    @display = row.render
-  end
-end
-
-game = Game.new(Board.new(Row.new(4)))
-game.board.render
+game = Game.new(Board.new)
+# game.board.render
+feedback = Feedback.new(4)
+game.start
