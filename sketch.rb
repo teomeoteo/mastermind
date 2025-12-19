23fa10868f98cecea30d4ff611c3fea56219afb6
teomac <liquidinobusiness@gmail.com>
2025-12-19 02:37:11 +0100

    Make row class that board uses to draw the entire board

--- sketch.rb
+++ sketch.rb
@@ -5,45 +5,89 @@ COLORS = {
   'yellow' => "\e[33m●\e[0m",
   'purple' => "\e[35m●\e[0m",
   'orange' => "\e[38;5;208m●\e[0m",
-  'empty'  => "○"
 }.freeze
 
 class Game
+  attr_accessor :board, :secret_code
   def initialize(board)
     @board = board
+    @secret_code = set_code
   end
 
   def start
+  end
+
+  def set_code
+    COLORS.keys.sample(board.rows[0].holes.length)
   end
 end
 
 class Player
 end
 
-class Board
+class Row
   attr_accessor :holes
   def initialize(hole_count) # number of holes
-    @holes = make_holes(hole_count.to_int)
+    @holes = make(hole_count.to_int)
   end
 
-  def make_holes(holes)
+  def make(holes)
     total_holes = []
     holes.times do
       total_holes << Hole.new('empty')
     end
     total_holes
   end
-  
+
+  def render
+    holes.each do |hole|
+      hole.render
+    end
+    print "\n"
+  end
+
   Hole = Struct.new(:state) do 
     def update_state(state)
-      self.state = COLORS.fetch(state)
+      self.state = state
     end
     
-    def print
-      
+    def render
+      if (self.state == 'empty')
+        print "○ "
+      else
+        print "#{COLORS[self.state]} "
+      end
     end
   end
 end
 
-class Feedback
+class Board
+  attr_accessor :rows
+  def initialize(row = Row.new(4))
+    @rows = make(row)
+  end
+  def render
+    rows.each do |row|
+      row.render
+    end
+  end
+
+  def make(row)
+    Array.new(12) {row}
+  end
 end
+
+class Player
+  def input(board)
+    puts "Choose 4 colors in order 1 by 1"
+  end
+end
+
+class Feedback
+  def initialize(row = Row.new(4))
+    @display = row.render
+  end
+end
+
+game = Game.new(Board.new(Row.new(4)))
+game.board.render
